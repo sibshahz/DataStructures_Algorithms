6da4b420bf0acfe66c47021062d5a9a2c215d5fa
Shahid Gillani <shahid5ssg@gmail.com>
2024-02-17 23:05:12 +0500

    anagram implementation

--- leetcode/Arrays_and_Hashing/anagram.js
+++ leetcode/Arrays_and_Hashing/anagram.js
@@ -1,18 +1,59 @@
+// var isAnagram = function(s, t) {
+//     if(s.length!==t.length)
+//         return false;
+//     s=s.toLowerCase()
+//     t=t.toLowerCase()
+//     const arrS=new Array(26).fill(0);
+//     const arrT=new Array(26).fill(0);
+//     for(let i=0;i<s.length;i++)
+//         arrS[s.charCodeAt(i)-97]+=1;
+    
+//     for(let i=0;i<t.length;i++)
+//         arrT[t.charCodeAt(i)-97]+=1;
+    
+    
+//     return JSON.stringify(arrS) === JSON.stringify(arrT);
+
+// };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
 var isAnagram = function(s, t) {
-    if(s.length!==t.length)
+    // a=s
+    // b=t
+    // join one seet of a+b;
+    // if length of set === a then you have anagram
+    // otherwise you dont
+    if(s.length !== t.length)
         return false;
-    s=s.toLowerCase()
-    t=t.toLowerCase()
-    const arrS=new Array(26).fill(0);
-    const arrT=new Array(26).fill(0);
-    for(let i=0;i<s.length;i++)
-        arrS[s.charCodeAt(i)-97]+=1;
-    
-    for(let i=0;i<t.length;i++)
-        arrT[t.charCodeAt(i)-97]+=1;
-    
-    
-    return JSON.stringify(arrS) === JSON.stringify(arrT);
+
+    const objS={}
+    const objT={}
+
+    for(let i = 0; i< s.length; i ++){
+        if(objS[s[i]]){
+            objS[s[i]]=objS[s[i]]+1
+        }else{
+            objS[s[i]]=1;
+        }
+    }
+
+    for(let i = 0; i< t.length; i ++){
+        if(objT[t[i]]){
+            objT[t[i]]=objT[t[i]]+1
+        }else{
+            objT[t[i]]=1;
+        }
+    }
+    let anagram=true;
+    Object.keys(objS).map((item) => {
+        if(objS[item] !== objT[item])
+            anagram=false;
+    })
+    return anagram;
 
 };
 
